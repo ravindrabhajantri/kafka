2dd3168f05a7d0b2077fcd3eb4b812d830e89f61
ravindrabhajantri <ravindra.bhaj@gmail.com>
2021-07-22 17:23:34 +0530

    Adding main class
    adding simple topic creation java class

---
+++ standalone-examples/create-topic/src/main/java/com/rav/bhaj/kafka/topics/CreateTopic.java
@@ -0,0 +1,59 @@
+package com.rav.bhaj.kafka.topics;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+import java.util.concurrent.ExecutionException;
+
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.ListTopicsResult;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.common.config.TopicConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CreateTopic {
+
+	private static final String TOPIC_NAME = "test-topic";
+	private static final int TOPIC_PARTITION = 1;
+	private static final short TOPIC_REPLICATION = 1;
+	private final static Logger LOGGER = LoggerFactory.getLogger(CreateTopic.class);
+
+	public static void main(String[] args) {
+
+		Properties kafkaProperties = new Properties();
+		kafkaProperties.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092");
+		kafkaProperties.put(AdminClientConfig.RETRIES_CONFIG, 3);
+
+		Map<String, String> topicConfig = new HashMap<String, String>();
+		topicConfig.put(TopicConfig.MIN_IN_SYNC_REPLICAS_CONFIG, "2");
+		topicConfig.put(TopicConfig.RETENTION_MS_CONFIG, "-1");
+
+		AdminClient adminClient = AdminClient.create(kafkaProperties);
+
+		final NewTopic newTopic = new NewTopic(TOPIC_NAME, TOPIC_PARTITION, TOPIC_REPLICATION);
+		newTopic.configs(topicConfig);
+
+		List<NewTopic> topics = new ArrayList<NewTopic>();
+		topics.add(newTopic);
+
+		ListTopicsResult listTopics = adminClient.listTopics();
+		Set<String> existingTopics;
+		try {
+			existingTopics = listTopics.names().get();
+			if (!existingTopics.contains(TOPIC_NAME)) {
+				adminClient.createTopics(topics);
+				LOGGER.info("Created topic with  name: {}, partitions: {}, replication: {}", TOPIC_NAME,
+						TOPIC_PARTITION, TOPIC_REPLICATION);
+			} else {
+				LOGGER.info("Topic with name {} already exists", TOPIC_NAME);
+			}
+		} catch (InterruptedException | ExecutionException e) {
+			LOGGER.error("Failed to create topic {} : {}", TOPIC_NAME, e.getMessage());
+		}
+	}
+}
